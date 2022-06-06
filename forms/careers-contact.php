699e7f84c30d6f568fd32199b126814485031011
Glenmhar Aurelio <89145761+Glenny07@users.noreply.github.com>
2022-06-06 16:14:21 +0800

    Add files via upload

---
+++ forms/careers-contact.php
@@ -0,0 +1,19 @@
+<?php
+ 
+$to = "careers@aclancpas.com";
+$subject = $_POST['subject'];
+$message = "Name: ". $_POST['name']."\n".
+           "Email: <".$_POST['email'].">\n\n".
+           $_POST['message'];
+$headers = "From: ".$_POST['email'];
+$sent= mail($to, $subject, $message, $headers);
+
+if ($sent) {
+  echo "<script>confirm('Your message sent successfully, we will get back to you soon.')</script>";
+}else{
+  echo "<script>confirm('We encountered an error sending an email, please try again later.')</script>";
+}
+?>
+<script>
+    window.history.back();
+</script>
